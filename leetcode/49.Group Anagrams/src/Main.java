d4e0374b9c8679d3b778df4a924440f9b41134b7
Finneas <1758843145@qq.com>
2026-02-03 14:26:34 +0800

    1

--- leetcode/49.Group Anagrams/src/Main.java
+++ leetcode/49.Group Anagrams/src/Main.java
@@ -1,7 +1,4 @@
 import java.util.List;
-
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public static void main(String[] args) {
     Solution demo = new Solution();
     String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
