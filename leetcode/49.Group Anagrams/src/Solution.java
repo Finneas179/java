fadb61b3fc16f18faf0c31de290f90c7e22a8aae
Finneas <1758843145@qq.com>
2026-02-03 14:25:10 +0800

    1

--- leetcode/49.Group Anagrams/src/Solution.java
+++ leetcode/49.Group Anagrams/src/Solution.java
@@ -1,16 +1,17 @@
 import java.util.*;
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>>/*一个list包裹一个list*/ groupAnagrams(String[] strs) {
         Map<String ,List<String>>map = new HashMap<>();
-        for(String s : strs){
-            char[]chars=s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String (chars);
+        //创建一个哈希表，前面的string是key，后面的是value值，就是说右边的申请一块内存来存储这个哈希表，然后
+        for(String s : strs){//这就是游历所有的strs所有值
+            char[]chars=s.toCharArray();//把每一个字符串分来成char
+            Arrays.sort(chars);//按照abcde的顺序排列
+            String key = new String (chars);//弄成一个新的然后赋值
             if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
+                map.put(key,new ArrayList<>());//没有就新建一个
             }
-            map.get(key).add(s);
+            map.get(key).add(s);//在对应的key添加对应的s
         }
-        return new ArrayList <>(map.values());
+        return new ArrayList <>(map.values());//返回return
     }
 }
